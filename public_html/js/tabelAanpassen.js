cea50649e4737219529f506449ee697ce04da847
Maarten Michiels <mnmmaarten@hotmail.com>
2018-05-09 21:21:46 +0200

    re-write transversing in jquery

--- public_html/js/tabelAanpassen.js
+++ public_html/js/tabelAanpassen.js
@@ -1,6 +1,6 @@
 $(function () {
     // script hier
     $('table').first().addClass('table-hover');
-    $('table tr:nth-child(4)').addClass('text-danger');
+    $('table tr').filter(':eq(3)').addClass('text-danger');
     $('table tr:nth-child(8)').addClass('text-danger');
 });
